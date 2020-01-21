d35d0226f2f283193b168bf852986834f20921ce
pal_user <reply2ppg@yahoo.com>
2020-01-21 09:46:27 -0700

    Add Security and OAuth2

--- applications/registration-server/src/main/java/io/pivotal/pal/tracker/registration/App.java
+++ applications/registration-server/src/main/java/io/pivotal/pal/tracker/registration/App.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 import java.util.TimeZone;
 
@@ -17,6 +19,8 @@ import java.util.TimeZone;
     "io.pivotal.pal.tracker.registration"
 })
 @EnableEurekaClient
+@EnableResourceServer
+@EnableWebSecurity
 public class App {
     public static void main(String[] args) {
         TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
